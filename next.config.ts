4b997de8e24b40c6bfc10c22b0bb1a0f45b9c249
Bobo Liu <719744328ccs@gmail.com>
2026-04-14 14:19:06 -0400

    next config
    next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  output: 'export',
-  basePath: '/bobo_portfolio',
-  images: { unoptimized: true },
+
 };
 module.exports = nextConfig;
 
